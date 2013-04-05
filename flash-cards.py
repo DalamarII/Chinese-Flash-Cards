2232cd2a30873d8c34c76ce9af5c0a334bd477a5
Jimson Xu <jimson.xu@gmail.com>
2013-04-05 14:05:31 -0700

    Add main program file. Very basic. Only contains a simple example dictionary.

---
+++ flash-cards.py
@@ -0,0 +1,10 @@
+dictionary = {'你好' : 'hello'}
+#dictionary = {'hello':'goodbye'};
+
+for c,e in dictionary.items():
+    print(c)
+    print(e)
+
+print("Please translate");
+zi = input("你阿訇: ")
+print("You entered " + zi)
