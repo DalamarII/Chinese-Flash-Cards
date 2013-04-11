b204d0857f3373c33c96df3bb5857d11d4617f6f
Jimson Xu <jimson.xu@gmail.com>
2013-04-10 23:25:05 -0700

    Allow input of alternate definitions without specifying a type. This allows for convenient shorthand such as:
    n. defn A/defn B
    
    Both defns would have a type of noun.
    
    Pretty large change. Had to change the dictionary a bit. Now the element
    returned given a key, is a list of DictionaryEntrys. Where a
    DictionaryEntry is simply a grouping of phrase type and phrase, e.g.
    noun and 'hi'. Modify the code to handle this new dictionary type.
    
    Seems to work mostly like before with 2 caveats:
    1) You no longer need to input the phrase type, e.g. Noun. In fact, this
    will be considered wrong if you do. I am not sure if I will bring back
    this feature. It is kind of nice not having to type in phrase types.
    I'll see how I feel about this later.
    2) You can no longer enter in all the alternate definitions at once. As
    there is no parsing of the user input. It is a straight up string
    compare. I don't think it's a big deal right now. Getting one is
    sufficient for most purposes. Will file a bug but will probably be lower
    priority. There are a lot of other things to work on.

--- flash-cards.py
+++ flash-cards.py
@@ -15,7 +15,17 @@ def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
     return type('Enum', (), enums)
 
-PhraseType = enum('Noun', 'Verb', 'Adjective')
+class DefinitionEntry:
+    ''' A definition entry in the Chinese to English dictionary.
+ It's simply a convenient way of grouping together phrase type and phrase. '''
+    def __init__(self, type, phrase):
+        self.type = type
+        self.phrase = phrase
+
+    def __str__(self):
+        return self.type + '. ' + self.phrase
+
+PhraseType = enum(Noun = 'n', Verb = 'v', Adjective = 'adj', Idiom =  'i')
 
 file = open("definitions.txt", encoding='utf-8')
 
@@ -25,9 +35,29 @@ for line in file:
     entry = line.split('-')
     assert(len(entry) == 2)
     key = entry[0]
-    defn = entry[1]
+    english = entry[1]
 
-    dict[key] = defn.strip()
+    # Create a list of DefinitionEntrys
+    englishList = english.strip().split('/')
+
+    defnList = []
+    type = 0
+
+    # TODO: Not 100% sure but I think for loops go in order in lists
+    # This means we can rely on this sequential parsing of the loop
+    # to check for the presence or absence of phrase types
+    for d in englishList:
+        dList = d.split('.', 1)
+        if len(dList) > 1:
+            assert(len(dList) == 2)
+            type = dList[0]
+            phrase = dList[1].strip()
+        else:
+            phrase = dList[0].strip()
+
+        defnList.append(DefinitionEntry(type, phrase))
+    	    
+    dict[key] = defnList
 
 i = 1
 for phrase, defn in dict.items():
@@ -36,23 +66,21 @@ for phrase, defn in dict.items():
     attempt = input(str(i) + ')' + phrase + ': ')
     i = i+1
 
-    # TODO: Not 100% sure but I think for loops go in order in lists
-    # This means we can rely on this sequential parsing of the loop
-    # to check for the presence or absence of phrase types
-    defnList = defn.split('/')
-
-    numDefn = len(defnList)
+    numDefn = len(defn)
     numCorrect = 0
 
     # Sort word type
-
-    for d in defnList:
-        if attempt.lower() == d.lower():
+    for d in defn:
+        if attempt.lower() == d.phrase.lower():
             numCorrect = numCorrect + 1
 
     if numCorrect > 0:
         print("\tCorrect!")
         if numCorrect < numDefn:
-            print ("\tAlso means '" + defn + "'")
+            print ("\tAlso means:")
+            for d in defn:
+                print('\t' + str(d))
     else:
-        print ("\tWrong!" + "\t'" + defn + "'");
+        print ("\tWrong!");
+        for d in defn:
+            print('\t' + str(d))
